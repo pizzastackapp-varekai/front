828cd303c0ad9c571eb75041cd6d28d315a32ba0
“Serhii_Saviuk” <s.saviuk@apiko.com>
2022-11-01 01:25:40 +0200

    install storybook

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { App } from '../App'
+
+export default {
+	title: 'Example/App',
+	component: App,
+
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+} as ComponentMeta<typeof App>
+
+const Template: ComponentStory<typeof App> = args => <App />
+
+export const Primary = Template.bind({})

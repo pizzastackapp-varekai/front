fadda97f8070582c3771611c32ec7c8a29318314
“Serhii_Saviuk” <s.saviuk@apiko.com>
2022-11-02 00:20:35 +0200

    add home page

--- src/common/components/header/header.component.tsx
+++ src/common/components/header/header.component.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
 	return (
-		<div className="h-12 shadow-xl px-6">
+		<div className="h-12 shadow-xl px-6 mb-8">
 			<div className="flex items-center h-full">
 				<a href="/" className="text-xl font-semibold">
 					🍕 Pizza Stack

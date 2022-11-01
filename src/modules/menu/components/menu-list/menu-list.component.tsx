aed08d56d43540412f9bc7ddd15033c577db55c9
“Serhii_Saviuk” <s.saviuk@apiko.com>
2022-11-01 23:46:05 +0200

    add props to MenuList

--- src/modules/menu/components/menu-list/menu-list.component.tsx
+++ src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -1,11 +1,21 @@
-import pizzaMenu from '@app/mocks/pizza.json'
+import { FC } from 'react'
+import { Pizza } from '../../types/pizza'
+
 import { MenuItem } from '../menu-item/menu-item.component'
 
-export const MenuList = () => {
+interface MenuListProps {
+	items: Pizza[]
+}
+
+export const MenuList: FC<MenuListProps> = ({ items }) => {
 	return (
 		<div className="flex flex-wrap gap-10">
-			{pizzaMenu.map(({ image, ...pizza }) => (
-				<MenuItem {...pizza} imagePath={`/assets/pizza/${image}`} />
+			{items.map(({ image, ...pizza }) => (
+				<MenuItem
+					{...pizza}
+					imagePath={`/assets/pizza/${image}`}
+					key={`pizza-${pizza.id}`}
+				/>
 			))}
 		</div>
 	)
